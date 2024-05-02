c2e004d7bfec769ed6f4eff5f5427c3f32287287
Suرa <95271348+spectrerah@users.noreply.github.com>
2024-05-03 02:01:26 +0530

    Update LITERAL TABLE.py

--- LPCC PRACTICALS/LITERAL TABLE.py
+++ LPCC PRACTICALS/LITERAL TABLE.py
@@ -6,7 +6,7 @@ def getClass(operator):
             clas = i[1]
     return clas
 EMOT = [['STOP', 1, 0], ['ADD', 1, 1], ['SUB', 1, 2], ['MULT', 1, 3], ['MOVER', 1, 4], ['MOVEM', 1, 5], ['COMP', 1, 6], ['BC', 1, 7], ['DIV', 1, 8], ['READ', 1, 9], ['PRINT', 1, 10], ['START', 3, 1], ['END', 3, 2], ['ORIGIN', 3, 3], ['EQU', 3, 4], ['LTORG', 3, 5], ['DS', 2, 1], ['DC', 2, 2], ['AREG', 4, 1], ['BREG', 4, 2], ['CREG', 4, 3], ['DREG', 4, 4]]
-file = open(r"C:\Users\Lenovo\Desktop\STUDY\SEMESTER 6\LPCC\PRACTICAL EXAM\INPUT CODE 1.txt", 'r')
+file = open(r"INPUT CODE 1.txt", 'r')
 lines = file.readlines()
 tokens = []
 for line in lines:
