866eb438471351306a35a558f06744e68f5df527
Suرa <95271348+spectrerah@users.noreply.github.com>
2024-05-03 10:18:39 +0530

    Update pyunzip.py

--- pyunzip.py
+++ pyunzip.py
@@ -1,5 +1,15 @@
 #use python3 pyunzip.py KSK.zip
 #!/usr/bin/env python3
+
+#lex test.l
+#gcc lex.yy.c
+#./a.out
+
+#lex test.l
+#bison -dy test.y
+#gcc lex.yy.c y.tab.c -lm
+#./a.out
+
 import sys
 from zipfile import PyZipFile
 for zip_file in sys.argv[1:]:
